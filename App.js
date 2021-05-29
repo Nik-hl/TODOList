df67febf3ed6466b2b569009a41eb34f7216a4db
Nik-hl <coepnikhil@gmial.com>
2021-05-29 16:53:37 +0530

    Added alert and dismissing keyboard feature

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import Header from './Containers/Header'
 import Todoitem from './Containers/todoitemp'
 import AddTodo from './Containers/Addtodo'
@@ -20,6 +20,7 @@ export default function App() {
   }
 
   const submmithadler = (text) => {
+    if(text.length < 32){
     settodos((prevTodos) => {
       return [
         {
@@ -30,23 +31,30 @@ export default function App() {
       ]
     })
   }
+  else{
+    Alert.alert('OOps!!', 'Slow it down bro..title must not exceed 32 char', [{text: 'OK'}])
+  }
+}
 
   return (
-    <View style={styles.container}>
-      <Header />
-      <View style={styles.content}>
-        <AddTodo submithandler={submmithadler}/>
-        <View style={styles.list}>
-          <FlatList
-            data = {todos}
-            renderItem={({ item }) => (
-              <Todoitem item = {item} presshandler={presshandler} />
-            )}
-          />
+    <TouchableWithoutFeedback onPress={() => {
+        Keyboard.dismiss();
+    }}>
+        <View style={styles.container}>
+        <Header />
+        <View style={styles.content}>
+          <AddTodo submithandler={submmithadler}/>
+          <View style={styles.list}>
+            <FlatList
+              data = {todos}
+              renderItem={({ item }) => (
+                <Todoitem item = {item} presshandler={presshandler} />
+              )}
+            />
+          </View>
         </View>
       </View>
-      
-    </View>
+    </TouchableWithoutFeedback>
   )
 };
 
